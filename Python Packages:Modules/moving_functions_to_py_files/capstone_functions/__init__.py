bab33b68b7862444826028feae408624eeef6b25
jirvingphd <james.irving.phd@gmail.com>
2021-02-19 15:14:35 -0500

    Added many sections

---
+++ Python Packages:Modules/moving_functions_to_py_files/capstone_functions/__init__.py
@@ -0,0 +1,9 @@
+"""A collection of example functions for 081720FT cohort
+- James M. Irving, Ph.D.
+- james.irving.phd@gmail.com"""
+
+from capstone_functions import my_functions as mf
+from capstone_functions import lz
+
+# from capstone_functions.my_functions import *
+# from capstone_functions.lz import *
